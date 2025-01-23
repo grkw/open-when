bf153e235a716ca8f1dd5fd0b8868d5d57ff61ca
Grace Kwak <gracekwak0@gmail.com>
2025-01-23 14:23:24 -0500

    Show dropdown of all unique prompts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,18 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { supabase } from "@/utils/supabase/client";
-import LetterOpener from "@/components/letter_opener";
 import LetterBrowser from "@/components/letter_browser";
 import LetterEditor from "@/components/letter_editor";
 import { LetterProps } from "@/components/letter";
 
 export default function Home() {
 
-  const defaultPrompts = [
-    "you're feeling blue",
-    "you feel lost",
-    "you've messed up",
-    "you need a laugh",
-    "you're exhausted",
-    "you're pissed off",
-    "you feel lonely",
-    "you're stressed out",
-    "you're feeling insecure",
-    "you're bored",
-    "other"
-  ];
-
   const [view, setView] = useState('instructions');
   const [hasWrittenLetter, setHasWrittenLetter] = useState(false);
   const [openedLetters, setOpenedLetters] = useState<LetterProps[]>([]);
   const [unopenedLetters, setUnopenedLetters] = useState<LetterProps[]>([]);
-  const [unopenedCounts, setUnopenedCounts] = useState<number[]>(new Array(defaultPrompts.length).fill(-1));
-  const [openedCounts, setOpenedCounts] = useState<number[]>(new Array(defaultPrompts.length).fill(-1));
-  const [numCredits, setNumCredits] = useState(0);
-  // const [openedLetter, setOpenedLetter] = useState<LetterProps[]>([]);
-  // const [openedID, setOpenedID] = useState(0); 
+  const [unopenedCounts, setUnopenedCounts] = useState<number[]>(new Array());
+  const [openedCounts, setOpenedCounts] = useState<number[]>(new Array());
+  const [prompts, setPrompts] = useState<string[]>([]);
 
   useEffect(() => {
     const fetchLetters = async () => {
@@ -41,7 +24,6 @@ export default function Home() {
 
         if (openedResponse.error) {
           console.error('Error fetching opened letters:', openedResponse.error);
-          // setOpenedLetters(null);
         } else {
           console.log('Opened letters:', openedResponse.data);
           setOpenedLetters(openedResponse.data);
@@ -49,11 +31,11 @@ export default function Home() {
 
         if (unopenedResponse.error) {
           console.error('Error fetching unopened letters:', unopenedResponse.error);
-          // setUnopenedLetters(null);
         } else {
           console.log('Unopened letters:', unopenedResponse.data);
           setUnopenedLetters(unopenedResponse.data);
         }
+
       } catch (error) {
         console.error('Unexpected error:', error);
       }
@@ -63,58 +45,53 @@ export default function Home() {
   }, []);
 
   useEffect(() => {
-    const fetchCredits = async () => {
-      try {
-        const { data, error } = await supabase.from("credits").select().eq('id', 1);
-        if (error) {
-          console.error('Error fetching credits:', error);
-        } else if (data) {
-          setNumCredits(data[0]?.num_credits || 0);
-          console.log('credits', data[0]?.num_credits);
-        }
-      } catch (error) {
-        console.error('Unexpected error:', error);
+    if (unopenedLetters.length > 0 || openedLetters.length > 0) {
+
+      console.log("unopened Prompts", unopenedLetters.map(letter => letter.prompt));
+      console.log("opened Prompts", openedLetters.map(letter => letter.prompt));
+
+      const uniquePrompts = new Set([
+        ...unopenedLetters.map(letter => letter.prompt),
+        ...openedLetters.map(letter => letter.prompt)
+      ]);
+
+      setPrompts(Array.from(uniquePrompts));
+    }
+  }, [openedLetters, unopenedLetters]);
+
+  useEffect(() => {
+    console.log("unique Prompts", Array.from(prompts));
+
+    const unopenedCountsByPrompt = new Array(prompts.length).fill(0);
+    const openedCountsByPrompt = new Array(prompts.length).fill(0);
+
+    openedLetters.forEach(letter => {
+      const index = prompts.indexOf(letter.prompt);
+      if (index !== -1) {
+        openedCountsByPrompt[index]++;
+      } else {
+        console.log("error");
       }
-    };
+    })
 
-    fetchCredits();
-  }, []); // Ensure the dependency array is present
+    unopenedLetters.forEach(letter => {
+      const index = prompts.indexOf(letter.prompt);
+      if (index !== -1) {
+        unopenedCountsByPrompt[index]++;
+      } else {
+        console.log("error");
+      }
+    })
 
-  useEffect(() => {
-    if (unopenedLetters) {
-      const promptCounts = new Array(defaultPrompts.length).fill(0);
-      unopenedLetters.forEach(letter => {
-        const index = defaultPrompts.indexOf(letter.prompt);
-        if (index !== -1) {
-          promptCounts[index]++;
-        } else {
-          promptCounts[promptCounts.length - 1]++;
-        }
-      });
-      setUnopenedCounts(promptCounts);
-      console.log("unopened counts", promptCounts);
-    }
-  }, [unopenedLetters]); //unopenedLetters, 
+    setOpenedCounts(openedCountsByPrompt);
+    setUnopenedCounts(unopenedCountsByPrompt);
+    console.log("unopened counts", unopenedCountsByPrompt);
+    console.log("opened counts", openedCountsByPrompt);
 
-
-  useEffect(() => {
-    if (openedLetters) {
-      const promptCounts = new Array(defaultPrompts.length).fill(0);
-      openedLetters.forEach(letter => {
-        const index = defaultPrompts.indexOf(letter.prompt);
-        if (index !== -1) {
-          promptCounts[index]++;
-        } else {
-          promptCounts[promptCounts.length - 1]++;
-        }
-      });
-      setOpenedCounts(promptCounts);
-      console.log("opened counts", promptCounts);
-    }
-  }, [openedLetters]); //openedLetters, 
+  }, [prompts]);
 
   const numUnopenedLetters = unopenedLetters ? unopenedCounts.reduce((partialSum, a) => partialSum + a, 0) : NaN;
-
+  const numOpenedLetters = openedLetters ? openedCounts.reduce((partialSum, a) => partialSum + a, 0) : NaN;
 
   return (
     <div className="wrapper">
@@ -123,25 +100,22 @@ export default function Home() {
       {view === 'instructions' && <div>
         <h2>about</h2>
         <p>this is a community-driven repository of digital "open when..." letters. an "open when..." letter is a letter meant to be opened when "..." (i.e. "open when sad," "open when tired," "open when you need a laugh"). usually, "open when..." letters are written between people who already know each other to keep in touch and feel close in the face of long distance separation. however, I think that the idea is still applicable to internet strangers - we may not know each other, but as fellow human beings, we can still send kindness and well-wishes to each other.</p>
-        <br /> 
+        <br />
         <h2>getting started</h2>
         <p>welcome, internet friend! how are you feeling?</p>
         <br />
-        <p>want some advice on how to navigate a particular feeling? you can <button onClick={() => setView('browse')}>browse letters</button> that have been written by past site visitors. there are currently <b>{0} opened letters</b> (which another person has opened and signed, and now anyone can read) and <b>{0} new letters</b> (letters that have yet to be opened and signed).</p>
+        <p>want some advice on how to navigate a particular feeling? you can <button onClick={() => setView('browse')}>browse letters</button> that have been written by past site visitors. as of now, there are currently <b>{numOpenedLetters} opened letters</b> (which another person has opened and signed, and now anyone can read) and <b>{numUnopenedLetters} new letters</b> (letters that have yet to be opened and signed).</p>
         <br />
         <p>want to provide emotional support to a fellow internet friend? you can <button onClick={() => setView('write')}>write a letter</button> for someone else to open. I find that the act of writing is immensely valuable in itself.</p>
         <br />
-        </div>}
+      </div>}
 
       {view === 'browse' && (
-        <LetterBrowser view={view} setView={setView} unopenedLetters={unopenedLetters}  openedLetters={openedLetters} openedCounts={openedCounts} unopenedCounts={unopenedCounts} defaultPrompts={defaultPrompts} />
+        <LetterBrowser view={view} setView={setView} unopenedLetters={unopenedLetters} openedLetters={openedLetters} openedCounts={openedCounts} unopenedCounts={unopenedCounts} defaultPrompts={prompts} />
       )}
       {view === 'write' && (
-        <LetterEditor setHasWrittenLetter={setHasWrittenLetter} setView={setView} defaultPrompts={defaultPrompts} numUnopenedLetters={numUnopenedLetters} />
+        <LetterEditor setHasWrittenLetter={setHasWrittenLetter} setView={setView} defaultPrompts={prompts} numUnopenedLetters={numUnopenedLetters} />
       )}
-      {/* {view === 'open' && (
-        <LetterOpener setView={setView} openedLetter={openedLetter} openedID={openedID} />
-      )} */}
 
     </div>
   );
