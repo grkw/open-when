ac22fef47438d79ab385e8c02c080ac7cebade16
Grace Kwak <gracekwak0@gmail.com>
2025-01-24 22:05:38 -0500

    Add favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -130,7 +130,7 @@ export default function Home() {
       )}
 
     <div className='footer'>
-    <p>made by <u><a href="https://gracekwak.me/">Grace</a></u> (she/her) at the <u><a href="https://www.recurse.com/">Recurse Center</a></u> in January 2025. a work in progress; planning to apply <u><a href="https://www.inkandswitch.com/local-first/">local-first</a></u> principles to it and make some UI improvements. <u><a href="https://github.com/grkw/open-when/tree/main">source code</a></u>.</p>
+    <p>made by <u><a href="https://gracekwak.me/">Grace</a></u> (she/her) at the <u><a href="https://www.recurse.com/">Recurse Center</a></u> in January 2025. a work in progress. <u><a href="https://github.com/grkw/open-when/tree/main">source code</a></u>.</p>
     </div>
     </div>
   );
