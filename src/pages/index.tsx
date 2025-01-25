afb5f800053120865275acea13222e67c5522860
Grace Kwak <gracekwak0@gmail.com>
2025-01-24 21:54:27 -0500

    Add footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -94,20 +94,33 @@ export default function Home() {
 
   return (
     <div className="wrapper">
-      <h1>open when...</h1>
+      <div style={{ display: 'flex' }}>
+        <h1>open when...</h1>
+        <button style={{ marginTop: 'auto', marginBottom: '10px' }} onClick={() => setView('instructions')}>home</button>
+      </div>
+      {view === 'instructions' &&
+        <div>
+          <h2>about</h2>
+          <p>this is a community-driven repository of digital <i>open when</i> letters. an <i>open when</i> letter is a letter meant to be opened at a particular time or in a particular situation (i.e. open when sad, open when tired, open when you need a laugh). usually, <i>open when</i> letters are written between people who already know each other to keep in touch and feel close in the face of long distance separation. however, I think that the idea is still applicable to internet strangers - we may not know each other, but as fellow human beings, we can still send kindness and well-wishes to each other.</p>
+          <br />
+          <p>note: this is <i>not</i> meant to be a replacement for real-life human interaction! just a supplement perhaps.</p>
 
-      {view === 'instructions' && <div>
-        <h2>about</h2>
-        <p>this is a community-driven repository of digital <i>open when</i> letters. an <i>open when</i> letter is a letter meant to be opened at a particular time or in a particular situation (i.e. open when sad, open when tired, open when you need a laugh). usually, <i>open when</i> letters are written between people who already know each other to keep in touch and feel close in the face of long distance separation. however, I think that the idea is still applicable to internet strangers - we may not know each other, but as fellow human beings, we can still send kindness and well-wishes to each other.</p>
-        <br />
-        <h2>getting started</h2>
-        <p>welcome, internet friend! how are you feeling?</p>
-        <br />
-        <p>want some advice on how to navigate a particular feeling? you can <button onClick={() => setView('browse')}>browse letters</button> that have been written by past site visitors. as of now, there are currently <b>{numOpenedLetters} opened letters</b> (which another person has opened and signed, and now anyone can read) and <b>{numUnopenedLetters} new letters</b> (letters that have yet to be opened and signed).</p>
-        <br />
-        <p>want to provide emotional support to a fellow internet friend? you can <button onClick={() => setView('write')}>write a letter</button> for someone else to open. I find that the act of writing is immensely valuable in itself.</p>
-        <br />
-      </div>}
+          <h2>getting started</h2>
+          <p>welcome, internet friend! how are you feeling?</p>
+          <br />
+          <p>want some advice on how to navigate a particular feeling? you can <button onClick={() => setView('browse')}>browse letters</button> that have been written by past site visitors. as of now, there are currently <b>{numOpenedLetters} opened letters</b> (which another person has opened and signed, and now anyone can read) and <b>{numUnopenedLetters} new letters</b> (letters that have yet to be opened and signed).</p>
+          <br />
+          <p>want to provide emotional support to a fellow internet friend? you can <button onClick={() => setView('write')}>write a letter</button> for someone else to open. I find that the act of writing is immensely valuable in itself.</p>
+
+
+          {/* <h2>the making of</h2> */}
+          <br />
+          <br />
+          <br />
+          {/* <p style={{marginBottom: '0px', width: '100vw'}}>made by <u><a href="https://gracekwak.me/">Grace</a></u> (she/her) at the <u><a href="https://www.recurse.com/">Recurse Center</a></u> in January 2025. (<u><a href="https://github.com/grkw/open-when/tree/main">source code</a></u>). still a work in progress; I&apos;m planning to apply <u><a href="https://www.inkandswitch.com/local-first/">local-first</a></u> principles to it and make some UI improvements.</p> */}
+        
+        </div>
+      }
 
       {view === 'browse' && (
         <LetterBrowser view={view} setView={setView} unopenedLetters={unopenedLetters} openedLetters={openedLetters} openedCounts={openedCounts} unopenedCounts={unopenedCounts} defaultPrompts={prompts} />
@@ -116,6 +129,9 @@ export default function Home() {
         <LetterEditor setView={setView} defaultPrompts={prompts} numUnopenedLetters={numUnopenedLetters} />
       )}
 
+    <div className='footer'>
+    <p>made by <u><a href="https://gracekwak.me/">Grace</a></u> (she/her) at the <u><a href="https://www.recurse.com/">Recurse Center</a></u> in January 2025. a work in progress; planning to apply <u><a href="https://www.inkandswitch.com/local-first/">local-first</a></u> principles to it and make some UI improvements. <u><a href="https://github.com/grkw/open-when/tree/main">source code</a></u>.</p>
+    </div>
     </div>
   );
 }
